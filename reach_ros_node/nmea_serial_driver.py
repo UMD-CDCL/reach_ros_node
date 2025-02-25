562362e7e7260e4f25ddc17b1a134b3055a2bd4f
Zach Bortoff <zbortoff@gmail.com>
2025-02-25 17:57:03 -0500

    added diagnostics messages to nmea_serial_driver

--- reach_ros_node/nmea_serial_driver.py
+++ reach_ros_node/nmea_serial_driver.py
@@ -6,6 +6,8 @@ from rclpy.node import Node
 import glob
 import serial
 import pyudev
+import diagnostic_updater
+import diagnostic_msgs
 
 
 class ros2_ReachSerialHandler(Node):
@@ -16,6 +18,7 @@ class ros2_ReachSerialHandler(Node):
         self.emlid = serial.Serial()
         self.device_connected = False
         self.context_ = pyudev.Context()
+        self.driver = None
         self.declare_parameters(
             namespace='',
             parameters=[
@@ -26,6 +29,29 @@ class ros2_ReachSerialHandler(Node):
                 ('use_rostime', True),
                 ('use_rmc', False)]
         )
+
+    def produce_diagnostics(self, stat):
+        # define nominal diagnostic status
+        status = diagnostic_msgs.msg.DiagnosticStatus.OK
+        summary_msg = 'GPS connected.'
+
+        # check if gps is connected
+        if not self.device_connected or self.driver is None:
+            status = diagnostic_msgs.msg.DiagnosticStatus.ERROR
+            summary_msg = 'GPS not connected'
+            stat.add('connected_to_gps', 'false')
+            stat.add('gps_has_fix', 'false')
+        else:
+            stat.add('connected_to_gps', 'true')
+            if not self.driver.has_fix:
+                summary_msg = 'GPS connected, but has no fix'
+                status = diagnostic_msgs.msg.DiagnosticStatus.WARN
+                stat.add('gps_has_fix', 'false')
+            else:
+                stat.add('gps_has_fix', 'true')
+
+        stat.summary(status, summary_msg)
+        return stat
 
     # Should open the connection and connect to the device
     # This will then also start publishing the information
@@ -38,7 +64,7 @@ class ros2_ReachSerialHandler(Node):
             return
 
         try:
-            driver = reach_ros_node.driver.RosNMEADriver(self)
+            self.driver = reach_ros_node.driver.RosNMEADriver(self)
         except Exception as e:
             self.get_logger().error("an error occured while trying to make the driver. Error was: %s." %e)
 
@@ -47,7 +73,7 @@ class ros2_ReachSerialHandler(Node):
                 data = self.emlid.readline()
                 rclpy.spin_once(self, timeout_sec=0) # allow functions such as ros2 node info, ros2 param list to work
                 try:
-                    driver.process_line(data.decode('utf-8').rstrip().encode('utf-8').strip(b'\x00'))
+                    self.driver.process_line(data.decode('utf-8').rstrip().encode('utf-8').strip(b'\x00'))
                 except ValueError as e:
                     self.get_logger().info("Value error, likely due to missing fields in the NMEA message. Error was: %s." % e)
         except Exception as e:
@@ -76,17 +102,20 @@ class ros2_ReachSerialHandler(Node):
 
 
 def main(args=None):
-	rclpy.init(args=args)
+    rclpy.init(args=args)
 
-	node = ros2_ReachSerialHandler()
-	
+    node = ros2_ReachSerialHandler()
+    updater = diagnostic_updater.Updater(node)
+    updater.setHardwareID('gps')
+    updater.add('diagnostics', node.produce_diagnostics)
 
-	# Start the nodes processing thread
-	node.start()
 
-	# at termination of the code (generally with ctrl-c) Destroy the node explicitly
-	node.destroy_node()
-	rclpy.shutdown()
+    # Start the nodes processing thread
+    node.start()
+
+    # at termination of the code (generally with ctrl-c) Destroy the node explicitly
+    node.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
 	main()
